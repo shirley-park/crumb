1d84e77d957f1160683e649cbf2729b0c7f17793
Shirley Park <shirley.park7@gmail.com>
2023-05-22 13:44:32 +1200

    add checkout steps

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,5 +1,41 @@
+import { useSelector } from 'react-redux'
+import { Box, Button, Stepper, Step, StepLabel } from '@mui/material'
+import { Formik } from 'formik'
+import { useState } from 'react'
+import * as yup from 'yup'
+import { shades } from '../../theme'
+
+const initialValues = {
+  billingAddress: {
+    firstName: '',
+    lastName: '',
+    streetAddress: '',
+    city: '',
+    postcode: '',
+    country: '',
+  },
+  email: '',
+  phone: '',
+}
+
 const Checkout = () => {
-  return <div>Checkout</div>
+  const [activeStep, setActiveStep] = useState(0)
+
+  return (
+    <Box width="40%" m="100px auto">
+      <Stepper activeStep={activeStep}>
+        <Step>
+          <StepLabel>Billing</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Shipping</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Payment</StepLabel>
+        </Step>
+      </Stepper>
+    </Box>
+  )
 }
 
 export default Checkout
