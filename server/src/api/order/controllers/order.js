00d161e53cd8eb48b8e75ff46421cf891bac8b94
Shirley Park <shirley.park7@gmail.com>
2023-05-29 17:21:44 +1200

    create session object in stripe, line items

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,9 +1,41 @@
-'use strict';
-
+"use strict";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
+    try {
+      // lineItems in Session object
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          };
+        })
+      );
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        customer_email: email,
+        mode: "payment",
+        success_url: `http://localhost:3000/checkout/success`,
+        cancel_url: `http://localhost:3000/`,
+      });
+    } catch (error) {}
+  },
+}));
