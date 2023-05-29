b324fa3d3ba81ddc5f3550f05bd0c57096d0737b
Shirley Park <shirley.park7@gmail.com>
2023-05-29 23:34:06 +1200

    complete checkout success page with button to return to store

--- client/src/scenes/checkout/CheckoutSuccess.jsx
+++ client/src/scenes/checkout/CheckoutSuccess.jsx
@@ -23,7 +23,7 @@ const CheckoutSuccess = () => {
               },
             }}
           >
-            continue shopping
+            Back to the store
           </Button>
         </a>
       </Box>
